e144dc4908be576d8fffa0c47bd09ea510e609e1
breakthatbass <gamache.taylor@gmail.com>
2020-05-07 10:29:47 -0400

    add comments and explanations of code

--- linkedlists/linkedlist.c
+++ linkedlists/linkedlist.c
@@ -21,17 +21,24 @@ void printlist(node_t *head)
 }
 
 node_t *create_new_node(int value)
+// this creates a new node in the linked list
+// adds to end of the list
 {
+  // allocate memory for a new node
   node_t *result = malloc(sizeof(node_t));
+  // set the value of the node to argument of function
   result->value = value;
+  // for now, just set it to point to nothing
   result->next = NULL;
   return result;
 }
 
 node_t *insert_at_head(node_t **head, node_t *node_to_insert)
 {
+  // have node_to_insert point to the head
   node_to_insert->next = *head;
-  *head = node_to_insert;
+  // now have the head point to the node_to_insert
+  *head = node_to_insert; // having this pointer requires the **head parameter
   return node_to_insert;
 }
 
@@ -56,6 +63,9 @@ node_t *find_node(node_t *head, int value)
 
 int main()
 {
+
+  /* create list through loop and functions *********/
+
   node_t *head = NULL;
   node_t *tmp;
 
@@ -71,6 +81,42 @@ int main()
   insert_after_node(tmp, create_new_node(75));
 
   printlist(head);
+
+// below is commented out so the above code will compile
+// it shows more manual ways to build a linked list
+
+  /* manually create the list *************/
+/*
+  node_t n1, n2, n3; // establish nodes...just three for now
+  node_t *head; // establish the head (starting point of list)
+
+  // set values for each node
+  n1.value = 45;
+  n2.value = 8;
+  n3.value = 32;
+
+  // link them up
+  // head = &n1; // set the head to point to the address of n1
+  n1.next = &n2; // set node n1 to point to node n2 (next node in list)
+  n2.next = &n3; // n2 to point to n3
+  n3.next = NULL; // set the last element in the list to point to NULL - indicating the end of the list
+
+*/
+  /* create list using the create_new_node function from above...still somewhat manual */
+
+/*
+  node_t *head; // establish starting point
+  node_t *tmp; // create temp var for setting and adding to list
+
+  tmp = create_new_node(32);
+  head = tmp; // set the node created as the beginning of list
+  tmp = create_new_node(8); //add another node
+  tmp = next->head; // make 8 point to head (32)
+  head = tmp; //now set 8 to head
+  tmp = create_new_node(34);
+  tmp = next->head;
+  head = tmp;
+*/
 
   return 0;
 }
