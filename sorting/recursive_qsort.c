2254aa69b6b913dc173edac53016714b1a97205b
breakthatbass <gamache.taylor@gmail.com>
2020-07-30 08:06:37 -0400

    add recursive quicksort

---
+++ sorting/recursive_qsort.c
@@ -0,0 +1,30 @@
+
+/* recursive version of quicksort */
+
+static void swap(int v[], int i, int j)
+{
+    int tmp;
+
+    tmp = v[i];
+    v[i] = v[j];
+    v[j] = tmp;
+}
+
+void qsort(int v[], int left, int right)
+{
+    int i, last;
+    
+    if (left >= right) {
+        return;// do nothing of array contains fewer than two elements
+    }
+    swap(v, left, (left + right)/2);
+    last = left;
+    for (i = left+1; i <= right; i++) {
+        if (v[i] < v[left]) {
+            swap(v, ++last, i);
+        }
+    }
+    swap(v, left, last);
+    qsort(v, left, last-1);
+    qsort(v, last+1, right);
+}
