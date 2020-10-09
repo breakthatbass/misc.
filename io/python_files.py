35f95c4d2deda90b898eb04e378aff6d5408542b
breakthatbass <gamache.taylor@gmail.com>
2020-10-09 17:59:12 -0400

    fix directory issue

--- io/python_files.py
+++ io/python_files.py
@@ -1,9 +1,8 @@
 '''
 Notes for working with files in python to keep as a reference
 
-taken from https://realpython.com/working-with-files-in-python/#toc
+taken from https:..realpythoncom.working-with-files-in-python.#toc
 '''
-
 
 import os  # for scandir() 
 
@@ -15,19 +14,19 @@ with open('test.txt', 'r') as f:
 
 
 # print a list of the dirs and files
-with os.scandir('cs50/') as entries:
+with os.scandir('.') as entries:
     # requires iteration as it is an object
     for entry in entries:
-       # print(entry.name)
+       # print(entryname)
         pass  # pass is necessary if the above print statement is commented out
 
 
 # print a list of only dirs, no files
-current_dir = 'cs50/'
+current_dir = '.'
 with os.scandir(current_dir) as entries:
     for entry in entries:
         if entry.is_dir(): # check if entry is a dir
-           # print(entry.name)
+           # print(entryname)
             pass
 
 
@@ -35,7 +34,7 @@ with os.scandir(current_dir) as entries:
 with os.scandir(current_dir) as entries:
     for entry in entries:
         if entry.is_file():  # check if entry is a file
-            #print(entry.name)
+            #print(entryname)
             pass
 
 
@@ -52,29 +51,32 @@ for file_name in os.listdir(current_dir):
 import glob
 from pathlib import Path
 
-p = Path('cs50/')
-for name in p.glob('*.md'):
+p = Path('.')
+for name in p.glob('*md'):
     #print(name)
     pass
 
 
 # Walking a directory tree and printing the names of the directories and files
-for dirpath, dirnames, files in os.walk('invoice/'):
+for dirpath, dirnames, files in os.walk('.'):
     print(f'Found directory: {dirpath}')
     for file_name in files:
         #print(file_name)
         pass
 
+#from pathlib import Path
+import pathlib
 
+def tree(directory):
+    '''print directory tree pattern'''
+    print(f'+ {directory}')
+    for path in sorted(directory.rglob('*')):
+        if path.name.startswith('', 0, 1) == False:
+            depth = len(path.relative_to(directory).parts)
+            spacer = '    ' * depth
+            print(f'{spacer}+ {pathname}')
 
-
-
-
-
-
-
-
-
+#tree(pathlibPathcwd())
 
 
 
