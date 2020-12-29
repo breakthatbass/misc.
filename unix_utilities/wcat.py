29365da100d887d9609bb869265f6be1caa607fa
breakthatbass <gamache.taylor@gmail.com>
2020-12-28 21:47:17 -0500

    add python version of wgrep with stdin capability

---
+++ unix_utilities/wcat.py
@@ -0,0 +1,22 @@
+import sys
+
+def main():
+
+    if (len(sys.argv) < 2):
+        for line in sys.stdin:
+            print(line, end='')
+
+
+    i = 1
+    while (i < len(sys.argv)):
+        try:
+            fp = open(sys.argv[i], 'r')
+            for line in fp:
+                print(line, end='')
+        except:
+            print('wcat: cannot open file')
+            sys.exit(1)
+        i+=1
+
+if __name__ == '__main__':
+    main()
