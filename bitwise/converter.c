bc9a23e9363fb5d74f14a54258f1684dc8e08a64
breakthatbass <gamache.taylor@gmail.com>
2021-01-02 12:42:20 -0500

    add more explicit comments in binary converter

--- bitwise/converter.c
+++ bitwise/converter.c
@@ -6,11 +6,20 @@
 // binary to decimal converter
 int btod(char *bin)
 {
-	int n = 0;
-	int count = 0;
+	int n = 0;		// the decimal number we will return
+	
+	/**
+	 * raise represent each decimal at each binary place
+	 * start with 1 and double it with each loop
+	 * 1, 2, 4, 8, 16, 32, 63, 255...
+	 * */
 	int raise = 1;
+
 	int len = strlen(bin)-1;
+	// we work back because we don't know how many bits
+	// might be in our binary number
 	for (int i = len; i >= 0; i--) {
+		// if there's a 1, add our current raise value to n
 		if (bin[i] == '1') n += raise;
 		raise *= 2;
 	}
