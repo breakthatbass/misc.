d143542e6d70ceccf313dc5ab9a947fe5b68facc
breakthatbass <gamache.taylor@gmail.com>
2021-01-11 13:56:58 -0500

    finish ht

--- hash_tab/hash_table.c
+++ hash_tab/hash_table.c
@@ -72,13 +72,80 @@ static int ht_get_hash (const char *s, const int num_buckets, const int attempt)
     return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
 
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
+
+// insert
+void ht_insert(ht_hash_table *ht, const char *key, const char *value)
+{
+	ht_item *item = ht_new_item(key, value);
+	int index = ht_get_hash(item->key, ht->size, 0);
+	ht_item *cur_item = ht->items[index];
+	int i = 1;
+	while (cur_item != NULL && cur_item != &HT_DELETED_ITEM) {
+		if (cur_item != &HT_DELETED_ITEM) {
+            if (strcmp(cur_item->key, key) == 0) {
+                ht_del_item(cur_item);
+                ht->items[index] = item;
+                return;
+            }
+        }
+		index = ht_get_hash(item->key, ht->size, i);
+        cur_item = ht->items[index];
+        i++;
+	}
+	ht->items[index] = item;
+    ht->count++;
+}
+
+
+char* ht_search(ht_hash_table* ht, const char* key) {
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item* item = ht->items[index];
+    int i = 1;
+    while (item != NULL) {
+        if (strcmp(item->key, key) == 0) {
+            return item->value;
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    } 
+    return NULL;
+}
+
+
+void ht_delete(ht_hash_table* ht, const char* key) {
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item* item = ht->items[index];
+    int i = 1;
+    while (item != NULL) {
+        if (item != &HT_DELETED_ITEM) {
+            if (strcmp(item->key, key) == 0) {
+                ht_del_item(item);
+                ht->items[index] = &HT_DELETED_ITEM;
+            }
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    } 
+    ht->count--;
+}
+
 int main()
 {
 	ht_hash_table *ht = ht_new();
-	ht_del_hash_table(ht);
 
-	int h = ht_hash("cat", 163, 53);
-	printf("%d\n", h);
+	char *s;
+	
+	ht_insert(ht, "taylor", "gamache");
+	s = ht_search(ht, "taylor");
+	printf("%s\n", s);
+	ht_delete(ht, "taylor");
+	s = ht_search(ht, "taylor");
+	if (s == NULL) {
+		printf("not in table\n");
+	}
 	
 	return 0;
 }
