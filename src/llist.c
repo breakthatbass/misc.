ce13874c272698cc2e45b08168c84e02166383af
breakthatbass <gamache.taylor@gmail.com>
2021-09-24 13:02:11 -0400

    create src dir for all src files

---
+++ src/llist.c
@@ -0,0 +1,347 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "llist.h"
+
+
+// list_init: initiate a linked list
+list_t *list_init(void)
+{
+    list_t *l = malloc(sizeof(list_t));
+    if (l) {
+        l->head = NULL;
+        l->nodes = 0;
+    }
+    return l;
+}
+
+
+/* *
+ *	new_node:
+ *	
+ *	DESCRIPTION:
+ *		allocate space for a new node in a list. if allocation is successful,
+ *		add the value to the node and connect it to list.
+ *
+ *	RETURN VALUE:
+ *		returns the node even if allocation is unsuccessful. error checking is
+ *		done outside of the function.
+ *
+ * */
+static node_t *new_node(int value, node_t *next)
+{
+    node_t *node = malloc(sizeof(node_t));
+
+    if (node != NULL) {
+        node->value = value;
+        node->next = next;
+    }
+    return node;
+}
+
+
+/************************************************
+ * ADDING ELEMENTS TO LIST
+ ***********************************************/
+
+
+/* *
+ *	push:
+ *
+ *	DESCRIPTION:
+ *		add a node to the front of the list
+ *
+ *	RETURN VALUE:
+ *		if allocation was succesful in new_node(), push will return 1. else, 0.
+ * */
+int push(list_t *l, int value)
+{
+    node_t *node = new_node(value, l->head);
+    int success = node != NULL;
+
+    if (success) {
+        l->head = node;
+        l->nodes++;
+    }
+    return success;
+}
+
+
+/* *
+ *	append:
+ *
+ *	DESCRIPTION:
+ *		add a node to the end of the list
+ *
+ *	RETURN VALUE:
+ *		if allocation was succesful in new_node(), append will return 1. else, 0.
+ *
+ *	MORE INFO:
+ *		append requires a check to see if the list is empty. if it is, we push
+ *		it to the beginning of the list. otherwise, we loop to the last node and
+ *		set the next pointer of that node to the new node. 
+ * */
+
+int append(list_t *l, int value)
+{
+    node_t *node = new_node(value, l->head);
+    int success = node != NULL;
+    node_t *tmp = l->head;
+    node->next = NULL;
+
+    if (!l->head) {
+        l->head = node;
+		l->head->next = NULL;
+	} else {
+        while (tmp->next != NULL)
+            tmp = tmp->next;
+        tmp->next = node;
+    }
+    l->nodes++;
+    return success;
+}
+
+/************************************************
+ * REMOVING ELEMENTS FROM LIST
+ ***********************************************/
+
+
+/* *
+ *	shift:
+ *
+ *	DESCRIPTION:
+ *		remove the last node in a list and return the value.
+ *
+ *	RETURN VALUE:
+ *		returns the last element in the list as an int, doesn't return the node.
+ *		if list is empty, -1 is returned.
+ **/
+int shift(list_t *l)
+{
+    int last = -1;
+    node_t *p = l->head;
+    node_t *prev = NULL;
+
+	// empty list
+	if (get_size(l) == 0)
+		return last;
+	else if (get_size(l) == 1) {
+        last = p->value;
+        l->head = NULL;
+    } else {
+        while (p->next) {
+            prev = p;
+            p = p->next;
+        }
+        last = p->value;
+        prev->next = NULL;
+    }
+    free(p);
+    l->nodes--;
+    return last;
+}
+
+
+/* *
+ *	pop:
+ *
+ *	DESCRIPTION:
+ *		remove the first node in a list and return the value.
+ *
+ *	RETURN VALUE:
+ *		returns the value in the first node in the list as an int, doesn't
+ *		return the node. if list is empty, -1 is returned.
+ **/
+int pop(list_t *l)
+{
+    int first = -1;
+	
+	if (get_size(l) == 0)
+		return first;
+
+    node_t *tmp = l->head;
+    first = tmp->value;
+
+    l->head = tmp->next;
+
+    free(tmp);
+    l->nodes--;
+    return first;
+}
+
+
+/* *
+ *	remove_node:
+ *
+ *	DESCRIPTION:
+ *		linearly search a list for a node containing target as its value. if
+ *		found, remove that node from the list.
+ *
+ *	RETURN VALUE:
+ *		if node with target was found 1 is returned, otherwise -1. 
+ **/
+int remove_node(list_t *l, int target)
+{
+    node_t **p = &l->head;
+    node_t *tmp = NULL;
+
+    while((*p)->next && (**p).value != target)
+        p = &(*p)->next;
+    if ((*p)->value == target) {
+        tmp = *p;
+        *p = tmp->next;
+        free(tmp);
+        l->nodes--;
+        return 1;
+    }
+    return -1;
+}
+
+
+
+/************************************************
+ * UTILITY FUNCTIONS
+ ***********************************************/
+
+/* *
+ *	search:
+ *
+ *	DESCRIPTION:
+ *		do a linear search for a node containing target as a value.
+ *
+ *	RETURN VALUE:
+ *		if value is found, it returns that value, otherwise -1.
+ **/
+int search(list_t *l, int target)
+{
+    node_t **p = &l->head;
+    while((*p)->next && (*p)->value != target)
+        p = &(*p)->next;
+
+    if ((*p)->value == target)
+        return target;
+    return -1;
+}
+
+
+/* *
+ *	destroy_list:
+ *
+ *	DESCRIPTION:
+ *		clean up and free entire list. no return value;
+ **/
+void destroy_list(list_t *l)
+{
+    node_t *cur = l->head;
+    node_t *next = NULL;
+
+    while (cur) {
+        next = cur->next;
+        free(cur);
+        cur = next;
+    }
+    free(l);
+}
+
+
+/* *
+ *	print_list:
+ *
+ *	DESCRIPTION:
+ *		print out the value in each node and number them. no return value;
+ **/
+void print_list(list_t *l)
+{
+    node_t *tmp = l->head;
+    int i = 1;
+
+    while (tmp) {
+        printf("%d: %d\n", i++, tmp->value);
+        tmp = tmp->next;
+    }
+}
+
+
+// get_size: return the number of nodes in a list
+size_t get_size(list_t *list)
+{
+    return list->nodes;
+}
+
+
+
+/* *
+ *	reverse:
+ *
+ *	DESCRIPTION:
+ *		reverse a linked list
+ *
+ *	RETURN VALUE:
+ *		look great in an interview
+ **/
+void reverse(list_t *l)
+{
+    node_t *prev = NULL;
+    node_t *cur = l->head;
+    node_t *next = l->head;
+
+    while (cur) {
+        next = next->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    l->head = prev;
+}
+
+
+/* for use in the qsort function below */
+static int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+/*	sort_list:
+ *
+ *	DESCRIPTION:
+ *		sort a linked list but creating an array to hold all elements
+ *      then using qsort on that array then looping through the array
+ *      and redefining the value held at each node from the array element.
+ * 
+ *      NOTE: this function does not preserve original list
+ **/
+void sort_list(list_t *l)
+{
+    // check if list is empty
+    if (l->nodes == 0) return;
+
+    // create array to hold each element in list
+    int list_size = l->nodes;
+    int *tmp_array = malloc(sizeof(int)*list_size);
+    if (tmp_array == NULL) return;
+
+    // load elements one by one into array
+    node_t *tmp = l->head;
+    int i = 0;
+    while (tmp) {
+        *tmp_array++ = tmp->value;
+        tmp = tmp->next;
+        i++;
+    }
+    tmp_array -= i;
+
+    // qsort it
+    qsort(tmp_array, list_size, sizeof(int), cmpfunc);
+    
+    // go through each element in list and reassign each node from array
+    i = 0;
+    tmp = l->head;
+    while (tmp) {
+        tmp->value = *tmp_array++;
+        tmp = tmp->next;
+        i++;
+    }
+    tmp_array -= i;
+    free(tmp_array);
+}
+
+
---
+++ src/split.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "split.h"
+
+static char *trim(char *str)
+{
+  char *end;
+  
+  // Trim leading space
+  while(isspace((unsigned char)*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  if (*end != ' ') return str;
+  while(end > str && isspace((unsigned char)*end)) end--;
+
+  // Write new null terminator character
+  end[1] = '\0';
+
+  return str;
+}
+
+// split: take a string and break it up into an array of strings based on delim
+char **split(char *s, const char *delim)
+{
+    char **split_s;
+    char *token, *tmp;
+    size_t length;
+    int i;
+
+	if (s == NULL || strcmp(s, "")== 0) return NULL;
+
+    length = strlen(s);
+
+	tmp = malloc(sizeof(char)*length+1);
+	if (tmp == NULL) return NULL;
+
+	strcpy(tmp, s);
+	tmp[length] = '\0';
+
+    split_s = calloc(length*2, sizeof(char*));
+    if (split_s == NULL) return NULL;
+
+    i = 0;
+    token = strtok(tmp, delim);
+    while (token != NULL) {
+		split_s[i] = trim(token);
+        token = strtok(NULL, delim);
+        i++;
+    }
+	if (i > 0)split_s[i] = NULL;
+    return split_s;
+}
+
+
+// print_arr: each string in an array of strings
+void print_arr(char **arr)
+{
+    int len = 0;
+    while (*arr) {
+        printf("%s\n", *arr);
+        len++;
+        arr++;
+    }
+    arr -= len;
+}
+
+
+// arr_len: get length of an array of strings
+int arr_len(char **arr)
+{
+    int l = 0;
+    while(*arr) {
+        arr++;
+        l++;
+    }
+    arr -= l;
+    return l;
+}
+
